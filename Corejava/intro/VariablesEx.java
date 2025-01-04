307c0891a8cdf2a9d26d36e171b6b353a4d21b6c
DLR2204 <n130488@rguktn.ac.in>
2025-01-04 22:23:22 +0530

    Add files via upload

---
+++ Corejava/intro/VariablesEx.java
@@ -0,0 +1,19 @@
+package intro;
+
+public class VariablesEx {
+
+	public static void main(String[] args) {
+		
+		
+		int value;  //declaration of a variable
+		
+		int number = 23434; //initialization of a variable
+		
+		
+		System.out.println(number);
+		
+		
+		
+		
+	}
+}
